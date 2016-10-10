cd71a0484129c26cc06e752b8f2264af79f13899
D--S <dawei.sun@gmail.com>
2016-10-10 15:34:13 +0800

    add more logs

--- app/src/main/java/com/testing/abcapplication/CActivity.java
+++ app/src/main/java/com/testing/abcapplication/CActivity.java
@@ -3,6 +3,8 @@ package com.testing.abcapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -23,6 +25,7 @@ public class CActivity extends AppCompatActivity implements View.OnClickListener
 	@Override
 	public void onClick(View v)
 	{
+		Log.i("CActivity", "Done button is clicked");
 		Intent intent = new Intent();
 		setResult(RESULT_OK, intent);
 		finish();
@@ -31,8 +34,24 @@ public class CActivity extends AppCompatActivity implements View.OnClickListener
 	@Override
 	public void onBackPressed()
 	{
+		Log.i("CActivity", "Hard Back button is pressed");
 		Intent intent = new Intent();
 		setResult(RESULT_CANCELED,intent);
 		super.onBackPressed();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case android.R.id.home:
+				Log.i("CActivity", "Back button on action bar is clicked");
+				Intent intent = new Intent();
+				setResult(RESULT_CANCELED, intent);
+				return super.onOptionsItemSelected(item);
+			default:
+				return super.onOptionsItemSelected(item);
+		}
 	}
 }
